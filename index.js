17e32a90798f4e064915b67394b64009ec9cb626
Angel de la Torre <ardelato@iMac.local>
2021-07-14 14:34:53 -0700

    Added comments and qa required zero parsing logic

--- index.js
+++ index.js
@@ -7,23 +7,24 @@ require("dotenv").config();
 const repos = require("./config.json").repos;
 const signatures = require("./config.json").signatures;
 
+// This is to add some flare and show the rate of which we are iterating through repos
 const bar = new ProgressBar("repos parsed [:bar] :percent", {
-    width: 100,
-    total: repos.length,
+  width: 100,
+  total: repos.length,
 });
 const timer = setInterval(function () {
-    bar.tick();
-    if (bar.complete) {
-        console.log("\ncomplete\n");
-        clearInterval(timer);
-    }
+  bar.tick();
+  if (bar.complete) {
+    console.log("\ncomplete\n");
+    clearInterval(timer);
+  }
 }, 100);
 
 // Sets Auth token for all future requests
 const ghqlAuthed = graphql.defaults({
-    headers: {
-        authorization: `token ${process.env.GITHUB_TOKEN}`,
-    },
+  headers: {
+    authorization: `token ${process.env.GITHUB_TOKEN}`,
+  },
 });
 
 const PULL_INFO = `
@@ -48,70 +49,83 @@ const GET_OPEN_PULLS = (repo, owner, limitsize) => `
     }
 `;
 
+// Automatically run script repeatedly
 (async () => {
-    main();
-    setInterval(main, 60 * 1000);
+  main();
+  setInterval(main, 60 * 1000);
 })();
 
 async function main() {
-    console.log("Running script...");
+  console.log("Running script...");
 
-    for (repo of repos) {
-        console.log("\tGetting pulls for " + repo.name);
-        const all_open_pulls = await ghqlAuthed(
-            GET_OPEN_PULLS(repo.name, repo.owner, 50) //Limiting it to 50 open pulls
-        );
-        await parsePulls(all_open_pulls.repository.pullRequests.nodes);
-        // bar.tick();
-        // if (bar.complete) {
-        //   console.log("\ncomplete\n");
-        // }
-    }
-    console.log("Finished script...");
+  // Iterate through the list of repos declared in the config.json file
+  for (repo of repos) {
+    console.log("\tGetting pulls for " + repo.name);
+    const all_open_pulls = await ghqlAuthed(
+      GET_OPEN_PULLS(repo.name, repo.owner, 50) //Limiting it to 50 open pulls
+    );
+    await parsePulls(all_open_pulls.repository.pullRequests.nodes);
+  }
+  console.log("Finished script...");
 }
 
 async function parsePulls(github_pulls) {
-    for (pull of github_pulls) {
-        getBaseProperties(pull);
-    }
+  for (pull of github_pulls) {
+    getBaseProperties(pull);
+  }
 }
 
+// Iteratres through the Pull Object and retrieves the appropriate base properties
 function getBaseProperties(pull) {
-    let build_status = pull.commits.nodes[0].commit.status
-        ? pull.commits.nodes[0].commit.status.state
-        : "EXPECTED";
-    if (build_status !== "EXPECTED" || build_status !== "SUCCESS") {
-        return;
-    }
+  let build_status = pull.commits.nodes[0].commit.status
+    ? pull.commits.nodes[0].commit.status.state
+    : "EXPECTED";
 
+  // Want to skip pulls that are failing CI
+  if (build_status !== "EXPECTED" || build_status !== "SUCCESS") {
+    return;
+  }
 
-    let tags = getTags(pull);
-    if(tags.includes('dev_block')){
-        return;
-    }
+  // Want to skip pulls that are dev_block
+  let tags = getTags(pull);
+  if (tags.includes("dev_block")) {
+    return;
+  }
 
-    
+  // Want to skip pulls that are marked as qa_req_0
+  let qa_req = qaRequired(pull);
+  if (!qa_req) {
+    return;
+  }
 }
 
-// Get Signaturse or Stamps
+// Get Signaturse/Stamps
 function getTags(pull) {
-    let latest_commit_date = new Date(pull.commits.nodes[0].commit.pushedDate);
-    let current_tags = [];
+  let latest_commit_date = new Date(pull.commits.nodes[0].commit.pushedDate);
+  let current_tags = [];
 
-    for (comment of pull.comments.nodes) {
-        let comment_date = new Date(comment.createdAt);
-        if (date.subtract(latest_commit_date, comment_date).toDays() <= 0) {
-            hasTags(comment.bodyText, current_tags);
-        }
+  for (comment of pull.comments.nodes) {
+    let comment_date = new Date(comment.createdAt);
+    if (date.subtract(latest_commit_date, comment_date).toDays() <= 0) {
+      hasTags(comment.bodyText, current_tags);
     }
-    return current_tags;
+  }
+  return current_tags;
 }
 
 function hasTags(comment, tags) {
-    signatures.tags.forEach((tag) => {
-        let regex = new RegExp(tag.regex + signatures.emoji, "i");
-        if (regex.test(comment)) {
-            tags.push(tag.name);
-        }
-    });
+  signatures.tags.forEach((tag) => {
+    let regex = new RegExp(tag.regex + signatures.emoji, "i");
+    if (regex.test(comment)) {
+      tags.push(tag.name);
+    }
+  });
+}
+
+// Check if the Pull requires QAing
+function qaRequired(pull) {
+  let body = pull.bodyText;
+  let qa_regex = new RegExp(signatures.qa_req, "i");
+
+  return qa_regex.test(body);
 }
