f6fa786503ef4c551ed2419e4a1ee2a527e3d9f9
Angel de la Torre <angel.dlt.1996@gmail.com>
2021-07-14 16:30:13 -0700

    build(deps): update .gitignore, index.js, package
    - changed config.json -> config.js for ES6 importing
    - modified importing of modules
    - commented out progress bar logic for the time being
    - appeneded console logs to keep track of counts
    - added 'const' to for loop iterator variable

--- index.js
+++ index.js
@@ -1,38 +1,40 @@
-require("dotenv").config();
+import dotenv from "dotenv";
+dotenv.config();
 
 import { graphql } from "@octokit/graphql";
 import ProgressBar from "progress";
-import { subtract } from "date-and-time";
-import { join } from "path";
+import date from "date-and-time";
+import path from "path";
 import { Low, JSONFile } from "lowdb";
 
-import { repos } from "./config.json";
-import { signatures } from "./config.json";
+import config from "./config.js";
+const repos = config.repos;
+const signatures = config.signatures;
 
-const file = path.join(__dirname, "db.json");
+const file = path.resolve("./db.json");
 const adapter = new JSONFile(file);
 const db = new Low(adapter);
 
 // Initialize JSON Database
 await db.read();
-db.data ||= [];
+db.data || (db.data = []);
 
 // Get Today's date
 let today = date.format(new Date(), "MM-DD-YYYY");
-let yesterday = date.addDays(today, -1);
+let yesterday = date.addDays(new Date(), -1);
 
 // This is to add some flare and show the rate of which we are iterating through repos
-const bar = new ProgressBar("repos parsed [:bar] :percent", {
-  width: 100,
-  total: repos.length,
-});
-const timer = setInterval(function () {
-  bar.tick();
-  if (bar.complete) {
-    console.log("\ncomplete\n");
-    clearInterval(timer);
-  }
-}, 100);
+// const bar = new ProgressBar("repos parsed [:bar] :percent", {
+//   width: 100,
+//   total: repos.length,
+// });
+// const timer = setInterval(function () {
+//   bar.tick();
+//   if (bar.complete) {
+//     console.log("\ncomplete\n");
+//     clearInterval(timer);
+//   }
+// }, 100);
 
 // Sets Auth token for all future requests
 const ghqlAuthed = graphql.defaults({
@@ -84,18 +86,20 @@ const GET_OPEN_PULLS = (repo, owner, limitsize) => `
 // Automatically run script repeatedly
 (async () => {
   main();
-  setInterval(main, 60 * 1000);
 })();
 
 async function main() {
-  let previous_pull_total = db.data[today] || db.data[yesterday] || 0;
+  let previous_pull_total =
+    db.data[today].day_total || db.data[yesterday].day_total || 0;
   let running_pull_total = 0;
-  let day_pull_count = 0;
+  let day_pull_count = db.data[today] ? db.data[today].pulls_added : 0;
   console.log("Running script...");
+  console.log("Previous Pull Total: " + previous_pull_total);
+  console.log("Repos in the list");
+  console.log(repos);
 
   // Iterate through the list of repos declared in the config.json file
-  for (repo of repos) {
-    console.log("\tGetting pulls for " + repo.name);
+  for (const repo of repos) {
     const all_open_pulls = await ghqlAuthed(
       GET_OPEN_PULLS(repo.name, repo.owner, 50) //Limiting it to 50 open pulls
     );
@@ -103,15 +107,22 @@ async function main() {
       all_open_pulls.repository.pullRequests.nodes
     );
   }
+  console.log("Running Total: " + running_pull_total);
 
-  day_pull_count += abs(running_pull_total - previous_pull_total);
-  previous_pull_total = running_pull_total;
+  let difference = running_pull_total - previous_pull_total;
+  day_pull_count += difference > 0 ? difference : 0;
+  console.log("New pulls added: " + day_pull_count);
+  db.data[today] = {
+    day_total: running_pull_total,
+    pulls_added: day_pull_count,
+  };
+  db.write();
   console.log("Finished script...");
 }
 
 function parsePulls(github_pulls) {
   let current_repo_pulls = 0;
-  for (pull of github_pulls) {
+  for (const pull of github_pulls) {
     current_repo_pulls += isQAReady(pull);
   }
   return current_repo_pulls;
@@ -124,21 +135,25 @@ function isQAReady(pull) {
     : "EXPECTED";
 
   // Want to skip pulls that are failing CI
-  if (build_status !== "EXPECTED" || build_status !== "SUCCESS") {
+  if (build_status !== "SUCCESS" && build_status !== "EXPECTED") {
+    console.log("CI is failing...returning 0");
     return 0;
   }
 
   // Want to skip pulls that are dev_block and already QA'd
   let tags = getTags(pull);
   if (tags.includes("dev_block") || tags.includes("QA")) {
+    console.log("dev_blocked or QA'd already...returning 0");
     return 0;
   }
 
   // Want to skip pulls that are marked as qa_req_0
   let qa_req = qaRequired(pull);
   if (!qa_req) {
+    console.log("Does not require QA...returning 0");
     return 0;
   }
+  console.log("Returning 1");
   return 1;
 }
 
@@ -147,10 +162,10 @@ function getTags(pull) {
   let latest_commit_date = new Date(pull.commits.nodes[0].commit.pushedDate);
   let current_tags = [];
 
-  for (comment of pull.comments.nodes) {
+  for (const comment of pull.comments.nodes) {
     let comment_date = new Date(comment.createdAt);
     // Only get tags associated with the latest commit
-    if (subtract(latest_commit_date, comment_date).toDays() <= 0) {
+    if (date.subtract(latest_commit_date, comment_date).toDays() <= 0) {
       hasTags(comment.bodyText, current_tags);
     }
   }
