b0f313e8c92401b7ed116c939e07b0732f22d990
Angel de la Torre <angel.dlt.1996@gmail.com>
2021-08-08 12:30:15 -0700

    Updated Pull Test file with unit tests for methods

--- db/db_pull.js
+++ db/db_pull.js
@@ -1,7 +1,7 @@
-import db from "./db_manager.js";
+import db from './db_manager.js'
 
-import logger from "../logger.js";
-const log = logger( 'db_pull' );
+import logger from '../logger.js'
+const log = logger('db_pull')
 
 const defaultData = {
   repo: '',
@@ -15,110 +15,103 @@ const defaultData = {
   closed_at: 0,
   merged_at: 0,
   closes: null,
-  interacted: false,
+  interacted: 0, //binmary boolean
   interacted_count: 0,
-  qa_ready: false,
+  qa_ready: 0, //binmary boolean
   qa_ready_count: 0,
-};
+}
 
-export default class Pull
-{
-  constructor( data )
-  {
-    if ( data )
-    {
-      this.data = { ...data };
-    } else
-    {
-      this.data = {
-        ...defaultData
-      };
+export default class Pull {
+  constructor(data) {
+    // Data passed is a GitHub Pull Object
+    if (data && data.number) {
+      this.data = { ...defaultData }
+      this.data.repo = data.headRepository.nameWithOwner
+      this.data.pull_number = data.number
+      this.data.title = data.title
+      this.data.head_ref = data.headRefOid
+      this.data.created_at = new Date(data.createdAt).getTime() / 1000
+      this.data.state = data.state
+    } else if (data) {
+      this.data = { ...data }
+    } else {
+      this.data = { ...defaultData }
     }
   }
-
-  gitInit( github_pull )
-  {
-    this.data.repo = github_pull.headRepository.nameWithOwner;
-    this.data.pull_number = github_pull.number;
-    this.data.title = github_pull.title;
-    this.data.head_ref = github_pull.headRefOid;
-    this.data.created_at = new Date( github_pull.createdAt ).getTime() / 1000;
-    this.data.state = github_pull.state;
-  }
-
-  // Retrieves the Repo and Pull Number in a formatted string 
-  getUniqueID()
-  {
-    return `${ this.data.repo } #${ this.data.pull_number }`;
+  // Retrieves the Repo and Pull Number in a formatted string
+  getUniqueID() {
+    return `${this.data.repo} #${this.data.pull_number}`
   }
 
   // Retrieves the Repo Owner, Repo Name, and Pull Number
-  getGraphQLValues()
-  {
-    let split = this.data.repo.split( '/' );
+  getGraphQLValues() {
+    let split = this.data.repo.split('/')
     let repo = {
-      name: split[ 1 ],
-      owner: split[ 0 ]
-    };
-    return [ repo, this.data.pull_number ];
+      name: split[1],
+      owner: split[0],
+    }
+    return [repo, this.data.pull_number]
   }
 
-
-  async save()
-  {
-    try
-    {
-      await db( 'qa_pulls' ).insert( { ...this.data } ).onConflict( "repo", "pull_number" ).merge();
-    } catch ( e )
-    {
-      log.error( "Failed to save Pull #%d '%s\n\t%s", this.data.pull_number, this.data.title, new Error( e.message ) );
+  async save() {
+    try {
+      await db('qa_pulls')
+        .insert({ ...this.data })
+        .onConflict('repo', 'pull_number')
+        .merge()
+    } catch (e) {
+      log.error(
+        "Failed to save Pull #%d '%s\n\t%s",
+        this.data.pull_number,
+        this.data.title,
+        new Error(e.message)
+      )
     }
   }
 
-  async setNewValues( data )
-  {
-    this.data = { ...data };
-    await this.save();
+  async setNewValues(data) {
+    this.data = { ...data }
+    await this.save()
   }
 
-  static async getDBPulls()
-  {
-    const rows = await db( 'qa_pulls' ).select().where( { state: "OPEN" } );
-    const db_pulls = [];
+  static async getDBPulls() {
+    const rows = await db('qa_pulls').select().where({ state: 'OPEN' })
+    const db_pulls = []
 
-    for ( let row of rows )
-    {
-      db_pulls.push( new Pull( row ) );
+    for (let row of rows) {
+      db_pulls.push(new Pull(row))
     }
 
-    return db_pulls;
+    return db_pulls
   }
 
-  static getSchemaJSON()
-  {
-    return defaultData;
+  static getSchemaJSON() {
+    return defaultData
   }
 
-  static async getQAReadyPullCount()
-  {
-    let result = await db( 'qa_pulls' ).count( 'qa_ready as running_pull_total' ).where( { 'qa_ready': true } );
-    return result[ 0 ].running_pull_total;
+  static async getQAReadyPullCount() {
+    let result = await db('qa_pulls')
+      .count('qa_ready as running_pull_total')
+      .where({ qa_ready: true })
+    return result[0].running_pull_total
   }
 
-  static async getQAReadyUniquePullCount()
-  {
-    let today = Math.floor( new Date().setHours( 0, 0, 0, 0 ) / 1000 );
-    let result = await db( 'qa_pulls' ).count( 'qa_ready_count as unique_pulls_added' ).where( 'qa_ready_count', '>', 0 ).andWhere( 'created_at', '>=', today );
-    log.data( 'Get Unique Pull Count Today\'s value: ' + today );
-    return result[ 0 ].unique_pulls_added;
+  static async getQAReadyUniquePullCount() {
+    let today = Math.floor(new Date().setHours(0, 0, 0, 0) / 1000)
+    let result = await db('qa_pulls')
+      .count('qa_ready_count as unique_pulls_added')
+      .where('qa_ready_count', '>', 0)
+      .andWhere('created_at', '>=', today)
+    log.data("Get Unique Pull Count Today's value: " + today)
+    return result[0].unique_pulls_added
   }
 
-  static async getInteractionsCount()
-  {
-    let today = Math.floor( new Date().setHours( 0, 0, 0, 0 ) / 1000 );
-    let result = await db( 'qa_pulls' ).count( 'interacted as pulls_interacted' ).where( { 'interacted': true } ).andWhere( 'updated_at', '>=', today );
-    return result[ 0 ].pulls_interacted;
+  static async getInteractionsCount() {
+    let today = Math.floor(new Date().setHours(0, 0, 0, 0) / 1000)
+    let result = await db('qa_pulls')
+      .count('interacted as pulls_interacted')
+      .where({ interacted: true })
+      .andWhere('updated_at', '>=', today)
+    return result[0].pulls_interacted
   }
-
 }
-
--- pullParser.js
+++ pullParser.js
@@ -1,160 +1,145 @@
-import date from "date-and-time";
-import Pull from './db/db_pull.js';
+import date from 'date-and-time'
+import Pull from './db/db_pull.js'
 
-import config from "./config/config.js";
-const SIGNATURES = config.signatures;
-const QA_TEAM = config.qa_team;
+import config from './config/config.js'
+const SIGNATURES = config.signatures
+const QA_TEAM = config.qa_team
 
-import logger from "./logger.js";
-const log = logger( 'pullParser' );
+import logger from './logger.js'
+const log = logger('pullParser')
 
-export default function parsePull( github_pull, db_pull = null )
-{
-  log.data( `Parsing Pull #${ github_pull.number } ${ github_pull.title }` );
-  if ( db_pull === null )
-  {
-    db_pull = new Pull();
-    db_pull.gitInit( github_pull );
+export default function parsePull(github_pull, db_pull = null) {
+  log.data(`Parsing Pull #${github_pull.number} ${github_pull.title}`)
+  if (db_pull === null) {
+    db_pull = new Pull(github_pull)
   }
-  const data = { ...db_pull.data };
+  const data = { ...db_pull.data }
 
-  updateDates( data, github_pull );
-  updateValues( data, github_pull );
+  updateDates(data, github_pull)
+  updateValues(data, github_pull)
 
-  db_pull.setNewValues( data );
+  db_pull.setNewValues(data)
 }
 
-function updateDates( db_pull_data, github_pull )
-{
-  db_pull_data.updated_at = formatGHDate( github_pull.updatedAt );
-  db_pull_data.closed_at = formatGHDate( github_pull.closedAt );
-  db_pull_data.merged_at = formatGHDate( github_pull.mergedAt );
+function updateDates(db_pull_data, github_pull) {
+  db_pull_data.updated_at = formatGHDate(github_pull.updatedAt)
+  db_pull_data.closed_at = formatGHDate(github_pull.closedAt)
+  db_pull_data.merged_at = formatGHDate(github_pull.mergedAt)
 }
 
-function formatGHDate( date )
-{
-  return Math.floor( new Date( date ).getTime() / 1000 );
+function formatGHDate(date) {
+  return Math.floor(new Date(date).getTime() / 1000)
 }
 
-function updateValues( db_pull_data, github_pull )
-{
+function updateValues(db_pull_data, github_pull) {
+  db_pull_data.head_ref = github_pull.headRefOid
+  db_pull_data.state = github_pull.state
 
-  db_pull_data.head_ref = github_pull.headRefOid;
-  db_pull_data.state = github_pull.state;
+  db_pull_data.closes = closesDeclared(github_pull)
 
-  db_pull_data.closes = closesDeclared( github_pull );
-
-  qaReadyAndInteracted( db_pull_data, github_pull );
+  qaReadyAndInteracted(db_pull_data, github_pull)
 }
 
 // Check if there is an Issue connected with Pull
-function closesDeclared( pull )
-{
-  let body = pull.bodyText;
-  let closes_regex = new RegExp( SIGNATURES.closes, 'i' );
-  let closes_pull = null;
-  let __CLOSE;
+function closesDeclared(pull) {
+  let body = pull.bodyText
+  let closes_regex = new RegExp(SIGNATURES.closes, 'i')
+  let closes_pull = null
+  let __CLOSE
 
-  if ( ( __CLOSE = body.match( closes_regex ) ) !== null )
-  {
-    closes_pull = parseInt( __CLOSE.groups.closes );
+  if ((__CLOSE = body.match(closes_regex)) !== null) {
+    closes_pull = parseInt(__CLOSE.groups.closes)
   }
-  return closes_pull;
+  return closes_pull
 }
 
-function qaReadyAndInteracted( db_pull_data, github_pull )
-{
-  let [ qa_ready, qa_interacted ] = isQAReadyAndInteracted( db_pull_data, github_pull );
-  log.data( `For Pull #${ db_pull_data.pull_number } ${ db_pull_data.title } Returned QA Ready: ${ qa_ready }, Current QA Ready: ${ db_pull_data.qa_ready }, Current QA Count: ${ db_pull_data.qa_ready_count },` );
+function qaReadyAndInteracted(db_pull_data, github_pull) {
+  let [qa_ready, qa_interacted] = isQAReadyAndInteracted(db_pull_data, github_pull)
+  log.data(
+    `For Pull #${db_pull_data.pull_number} ${db_pull_data.title} Returned QA Ready: ${qa_ready}, Current QA Ready: ${db_pull_data.qa_ready}, Current QA Count: ${db_pull_data.qa_ready_count},`
+  )
 
-  db_pull_data.qa_ready_count += !db_pull_data.qa_ready && qa_ready ? 1 : 0;
-  db_pull_data.qa_ready = qa_ready;
+  db_pull_data.qa_ready_count += !db_pull_data.qa_ready && qa_ready ? 1 : 0
+  db_pull_data.qa_ready = qa_ready
 
-  db_pull_data.interacted_count += !db_pull_data.interacted & qa_interacted ? 1 : 0;
-  db_pull_data.interacted = qa_interacted;
+  db_pull_data.interacted_count += !db_pull_data.interacted & qa_interacted ? 1 : 0
+  db_pull_data.interacted = qa_interacted
 }
 
 // Iteratres through the Pull Object and retrieves the appropriate base properties
-function isQAReadyAndInteracted( db_pull_data, github_pull )
-{
-  let build_status = github_pull.commits.nodes[ 0 ].commit.status
-    ? github_pull.commits.nodes[ 0 ].commit.status.state
-    : "EXPECTED";
+function isQAReadyAndInteracted(db_pull_data, github_pull) {
+  let build_status = github_pull.commits.nodes[0].commit.status
+    ? github_pull.commits.nodes[0].commit.status.state
+    : 'EXPECTED'
 
-  let qa_ready = true;
+  let qa_ready = true
   // Want to skip pulls that are marked as qa_req_0
-  let qa_req = qaRequired( github_pull );
-  if ( qa_req )
-  {
-    qa_ready = false;
-    db_pull_data.qa_req = false;
+  let qa_req = qaRequired(github_pull)
+  if (qa_req) {
+    qa_ready = false
+    db_pull_data.qa_req = false
   }
 
   // Want to skip pulls that are failing CI
-  if ( build_status !== "SUCCESS" && build_status !== "EXPECTED" )
-  {
-    qa_ready = false;
+  if (build_status !== 'SUCCESS' && build_status !== 'EXPECTED') {
+    qa_ready = false
   }
 
   // Want to skip pulls that are dev_block and already QA'd
-  let [ tags, qa_interacted ] = getTagsAndInteracted( github_pull );
-  if ( tags[ "dev_block" ] || tags[ "QA" ] )
-  {
-    qa_ready = false;
+  let [tags, qa_interacted] = getTagsAndInteracted(github_pull)
+  if (tags['dev_block'] || tags['QA']) {
+    qa_ready = false
   }
 
-  return [ qa_ready, qa_interacted ];
+  return [qa_ready, qa_interacted]
 }
 
 // Get Signaturse/Stamps
-function getTagsAndInteracted( github_pull )
-{
-  let latest_commit_date = new Date( github_pull.commits.nodes[ 0 ].commit.pushedDate );
-  let current_tags = {};
-  let interacted = false;
+function getTagsAndInteracted(github_pull) {
+  let latest_commit_date = new Date(github_pull.commits.nodes[0].commit.pushedDate)
+  let current_tags = {}
+  let interacted = false
 
-  for ( const comment of github_pull.comments.nodes )
-  {
-    let comment_date = new Date( comment.createdAt );
-    if ( hasQATag( comment.bodyText ) && date.subtract( latest_commit_date, comment_date ).toDays() <= 0 )
-    {
-      current_tags[ "QA" ] = true;
-    } else
-    {
-      hasTags( comment.bodyText, current_tags );
+  for (const comment of github_pull.comments.nodes) {
+    let comment_date = new Date(comment.createdAt)
+    if (
+      hasQATag(comment.bodyText) &&
+      date.subtract(latest_commit_date, comment_date).toDays() <= 0
+    ) {
+      current_tags['QA'] = true
+    } else {
+      hasTags(comment.bodyText, current_tags)
     }
 
-    if ( QA_TEAM.includes( comment.author.login ) && date.subtract( latest_commit_date, comment_date ).toDays() <= 0 && date.isSameDay( comment_date, new Date() ) )
-    {
-      interacted = true;
+    if (
+      QA_TEAM.includes(comment.author.login) &&
+      date.subtract(latest_commit_date, comment_date).toDays() <= 0 &&
+      date.isSameDay(comment_date, new Date())
+    ) {
+      interacted = true
     }
   }
 
-  return [ current_tags, interacted ];
+  return [current_tags, interacted]
 }
 
-function hasQATag( comment )
-{
-  let regex = new RegExp( SIGNATURES.QA + SIGNATURES.emoji, "i" );
-  return regex.test( comment );
+function hasQATag(comment) {
+  let regex = new RegExp(SIGNATURES.QA + SIGNATURES.emoji, 'i')
+  return regex.test(comment)
 }
 
-function hasTags( comment, tags )
-{
-  SIGNATURES.tags.forEach( ( tag ) =>
-  {
-    let regex = new RegExp( tag.regex + SIGNATURES.emoji, "i" );
-    if ( regex.test( comment ) )
-    {
-      tags[ "dev_block" ] = tag.state;
+function hasTags(comment, tags) {
+  SIGNATURES.tags.forEach(tag => {
+    let regex = new RegExp(tag.regex + SIGNATURES.emoji, 'i')
+    if (regex.test(comment)) {
+      tags['dev_block'] = tag.state
     }
-  } );
+  })
 }
 
 // Check if the Pull requires QAing
-function qaRequired( pull )
-{
-  let body = pull.bodyText;
-  let qa_regex = new RegExp( SIGNATURES.qa_req, "i" );
-  return qa_regex.test( body );
-};
+function qaRequired(pull) {
+  let body = pull.bodyText
+  let qa_regex = new RegExp(SIGNATURES.qa_req, 'i')
+  return qa_regex.test(body)
+}
