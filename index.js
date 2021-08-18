51cb9227bb34d68b7138ce8500ddfe7cc063819e
Angel de la Torre <angel.dlt.1996@gmail.com>
2021-08-18 15:37:27 -0700

    Remove accidental autosuggested line

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ const DB_PULLS = await Pull.getDBPulls()
 import parsePull from './pullParser.js'
 
 import logger from './logger.js'
-import { callbackify } from 'util'
 const log = logger('main')
 
 // Automatically run script repeatedly
