9c27b0bf42fa7ab8081ae98d7760788fe99c34f0
Angel de la Torre <angel.dlt.1996@gmail.com>
2021-09-14 16:12:08 -0700

    Removed knexfile since we won't use knex for migrations in the future add the connection configuration directly into knex.js

--- knex/knex.js
+++ knex/knex.js
@@ -1,4 +1,18 @@
-import config from './knexfile.js';
 import knex from 'knex';
+import path from 'path';
+import dotenv from 'dotenv';
 
-export default knex( config );
+dotenv.config({ path: path.resolve(`.env.${process.env.NODE_ENV || 'dev'}`) });
+
+const config = {
+  client: 'mysql',
+  connection: {
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+  },
+  debug: !!process.env.DEBUG, //converts to boolean value instead of truthy/falsy value
+};
+
+export default knex(config);
