e31599bd9b819b758d451d4ece3fe2c8d2f8c4d3
Angel de la Torre <angel.dlt.1996@gmail.com>
2021-09-15 13:10:45 -0700

    Added types declared in the global defintion file to ghgraphql.ts

--- src/ghgraphql.ts
+++ src/ghgraphql.ts
@@ -1,53 +1,65 @@
 import { graphql } from '@octokit/graphql'
 import config from '../config/graphql_config'
+
 const get_open_pulls = config.GET_OPEN_PULLS
 const get_pull = config.GET_PULL
 const get_issue = config.GET_ISSUE
 const get_day_issues = config.GET_DAY_ISSUES
 const get_issues = config.GET_ISSUES
 
-export async function queryPull(repo, pullNumber) {
-  return await graphql(get_pull(repo.name, repo.owner, pullNumber), {
-    headers: {
-      authorization: `token ${process.env.GITHUB_TOKEN}`,
+export async function queryPull(repo: { name: string, owner: string }, pullNumber: number):
+  Promise<GitHubRepositorySinglePull> {
+  return graphql(
+    get_pull(repo.name, repo.owner, pullNumber), {
+      headers: {
+        authorization: `token ${process.env.GITHUB_TOKEN}`,
+      },
     },
-  }).catch(error => console.error(error))
+  );
 }
 
-export async function queryIssue(repo, issueNumber) {
-  return await graphql(get_issue(repo.name, repo.owner, issueNumber), {
-    headers: {
-      authorization: `token ${process.env.GITHUB_TOKEN}`,
+export async function queryIssue(repo: { name: string, owner: string }, issueNumber: number) {
+  return graphql(
+    get_issue(repo.name, repo.owner, issueNumber), {
+      headers: {
+        authorization: `token ${process.env.GITHUB_TOKEN}`,
+      },
     },
-  })
+  );
 }
 
-export async function queryDayIssues(repo) {
-  const today = new Date()
-  today.setHours(0, 0, 0, 0)
+export async function queryDayIssues(repo: { name: string, owner: string }) {
+  const today = new Date();
+  today.setHours(0, 0, 0, 0);
 
-  return await graphql(get_day_issues(repo.name, repo.owner, 50, today.toISOString()), {
-    headers: {
-      authorization: `token ${process.env.GITHUB_TOKEN}`,
+  return graphql(
+    get_day_issues(repo.name, repo.owner, 50, today.toISOString()), {
+      headers: {
+        authorization: `token ${process.env.GITHUB_TOKEN}`,
+      },
     },
-  })
+  );
 }
 
-export async function queryIssues(repo, cursor = null) {
-  return await graphql(get_issues(repo.name, repo.owner, cursor), {
-    headers: {
-      authorization: `token ${process.env.GITHUB_TOKEN}`,
+export async function queryIssues(repo: { name: string, owner: string },
+  cursor: string | null = null) {
+  return graphql(
+    get_issues(repo.name, repo.owner, cursor), {
+      headers: {
+        authorization: `token ${process.env.GITHUB_TOKEN}`,
+      },
     },
-  })
+  );
 }
 
-export async function queryOpenPulls(repo) {
-  return await graphql(
+export async function queryOpenPulls(repo: { name: string, owner: string }):
+  Promise<GitHubRepositoryPulls> {
+  return graphql(
     get_open_pulls(repo.name, repo.owner, 50), //Limiting it to 50 open pulls,
     {
       headers: {
         authorization: `token ${process.env.GITHUB_TOKEN}`,
       },
-    }
-  )
+    },
+  );
 }
