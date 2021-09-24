e8a30534548519188b8371ab2353c0802e0b542b
Angel de la Torre <angel.dlt.1996@gmail.com>
2021-09-24 15:50:15 -0700

    Updating refresh pulls script

--- scripts/refreshPulls.ts
+++ scripts/refreshPulls.ts
@@ -1,17 +1,17 @@
 // Plan is to run this once to update all Pulls current in the DB that are still marked as OPEN yet the live Pull has been CLOSED or MERGED
-
-import Pull from '../db/db_pull'
+import { Pull} from "@prisma/client"
+import PullRequest from '../db/db_pull'
 import { queryPull } from '../src/ghgraphql'
-import parsePull from '../src/pullParser'
+import { parsePull } from '../controllers/pull_controller'
 
 
 import logger from '../src/logger'
 export default async function ():Promise<void> {
-  const db_pulls = await Pull.getDBPulls();
+  const db_pulls: Pull[] = await PullRequest.getDBPulls();
   const log = logger('refreshPull');
 
-  db_pulls.forEach(async (db_pull) => {
-    const github_pull = await queryPull(...db_pull.getGraphQLValues());
+  db_pulls.forEach(async (db_pull:Pull) => {
+    const github_pull = await queryPull(...PullRequest.getGraphQLValues(db_pull));
     parsePull(github_pull.repository.pullRequest, db_pull);
   });
 
