d2075477f9f91ecef3832c2057b3f0dee9c356f9
Angel de la Torre <angel.dlt.1996@gmail.com>
2021-09-24 10:03:49 -0700

    Now that 'Day' type has a 'date' property, use that to reference today's date

--- db/db_day.ts
+++ db/db_day.ts
@@ -13,7 +13,6 @@ export default class DayMetric
 {
   dayMetrics: Day
 
-  today: number;
   yesterday: number;
   constructor()
   {
@@ -21,28 +20,29 @@ export default class DayMetric
       pull_count: 0,
       pulls_added: 0,
       pulls_interacted: 0,
-      unique_pulls_added: 0
+      unique_pulls_added: 0,
+      date: 0
     };
     let [ t, y ] = utils.getDates();
-    this.today = t;
+    this.dayMetrics.date = t;
     this.yesterday = y;
-    log.data( `Today's values are ${ this.today } and yesterday is ${ this.yesterday }` );
+    log.data( `Today's values are ${ this.dayMetrics.date } and yesterday is ${ this.yesterday }` );
 
   }
 
 
   // Initial the day
   async init(): Promise<void> {
-    let day = await db( 'qa_metrics' ).first().where( { "date": this.today } ).orWhere( { "date": this.yesterday } ).orderBy( "date", "desc" );
+    let day = await db( 'qa_metrics' ).first().where( { "date": this.dayMetrics.date } ).orWhere( { "date": this.yesterday } ).orderBy( "date", "desc" );
 
     log.data( `Day Data ${ JSON.stringify( day, null, 2 ) }` );
 
     if ( day )
     {
       this.dayMetrics.pull_count = day.pull_count;
-      log.data( ` Today's date and yesterday's date ${ this.today } , ${ day.date }` );
-      log.data( ` Value for Pulls added is ${ this.today - day.date === TWENTY_FOUR_HOURS ? 0 : day.pulls_added } ` );
-      if ( this.today - day.date !== TWENTY_FOUR_HOURS )
+      log.data( ` Today's date and yesterday's date ${ this.dayMetrics.date } , ${ day.date }` );
+      log.data( ` Value for Pulls added is ${ this.dayMetrics.date - day.date === TWENTY_FOUR_HOURS ? 0 : day.pulls_added } ` );
+      if ( this.dayMetrics.date - day.date !== TWENTY_FOUR_HOURS )
       {
         this.dayMetrics.pulls_added = day.pulls_added;
         this.dayMetrics.pulls_interacted = day.pulls_interacted;
@@ -59,16 +59,16 @@ export default class DayMetric
   // Insert the new Day in the table and if it exists Update the values accordingly
   async save( newMetrics: Day | null = null ): Promise<void>
   {
-    if ( this.today !== Math.floor( new Date().setHours( 0, 0, 0, 0 ) / 1000 ) )
+    if ( this.dayMetrics.date !== Math.floor( new Date().setHours( 0, 0, 0, 0 ) / 1000 ) )
     {
-      [ this.today, this.yesterday ] = utils.getDates();
+      [ this.dayMetrics.date, this.yesterday ] = utils.getDates();
       this.dayMetrics.pulls_added = 0;
     }
     this.dayMetrics = newMetrics || this.dayMetrics;
     try
     {
       await db( 'qa_metrics' )
-        .insert( { "date": this.today, ...this.dayMetrics } )
+        .insert( { ...this.dayMetrics } )
         .onConflict( "date" ).merge();
     } catch ( e )
     {
