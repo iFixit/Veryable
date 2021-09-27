5954a9d796ec74bcf2dc6fe6d817b721536878cc
Angel de la Torre <angel.dlt.1996@gmail.com>
2021-09-27 13:46:47 -0700

    Installed dotenv-expand to cleanup env file variables
    Updated env template to match current configuration.
    
    Update Prsima Client configuration to make use of dotenv-expand

--- prisma/client.ts
+++ prisma/client.ts
@@ -2,10 +2,11 @@ import { PrismaClient } from '@prisma/client'
 
 import path from 'path';
 import dotenv from 'dotenv';
+import dotenvExpand from 'dotenv-expand';
 
-dotenv.config({ path: path.resolve(`.env.${process.env.NODE_ENV || 'dev'}`) });
+const env = dotenv.config({ path: path.resolve(`.env.${process.env.NODE_ENV || 'dev'}`) });
+dotenvExpand(env)
 
 const prisma = new PrismaClient();
-
 
 export default prisma
