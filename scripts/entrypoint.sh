6a5e67c89494c6f4237beb4fb78f499ef27b3d9d
Angel de la Torre <angel.dlt.1996@gmail.com>
2021-11-16 15:06:31 -0800

    fix(docker-compose): add entrypoint script for app service
    Upon starting up the application, the entrypoint script will be executed
    similar to Pulldasher-
    (https://github.com/iFixit/pulldasher/blob/master/docker-compose.yml#L6)
    (https://github.com/iFixit/pulldasher/blob/master/entrypoint.sh)
    
    This will wait until the MySQL Container is up and running. Then it will
    run `prisma:*_setup` which will run a kind of migration that will setup
    the dev db and test db. Without this, we cannot start the application,
    since the tables it calls do not exist.
    
    After the migration, it will run the main application--i.e. 'start'.

---
+++ scripts/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+echo -e "\033[1;34m====================== Setting Up DB =======================\e[0m\n"
+
+# Wait for the DB to start accepting connections.
+for i in $(seq 5); do
+    sleep 10
+    echo -e "\033[1;33m Attempt $i \e[0m"
+    npm run prisma:start_setup && npm run prisma:test_setup && break
+done
+
+echo -e "\033[1;32m======================== Setup Done ========================\e[0m\n"
+
+echo -e "\033[1;34m==================== Running Application ===================\e[0m"
+
+npm run start
