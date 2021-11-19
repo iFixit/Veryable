4172242ae56b066cedcb09df47841b4eacaaf421
Angel de la Torre <angel.dlt.1996@gmail.com>
2021-11-19 11:53:25 -0800

    refactor(shell-scripts): use ash busybox syntax
    Alpine docker images do not include bash so change shell scripts to
    properly use ash (busybox) syntax

--- scripts/docker-tests.sh
+++ scripts/docker-tests.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#!/bin/sh
 
 set -e
 
 docker exec -it veryable-app npm run test || \
-echo -e "\033[1;31m============= Error running tests in Docker Container ====================\n"
+printf "\033[1;31m============= Error running tests in Docker Container ====================\n"
