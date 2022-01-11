dde604f5a9dddc70d70eb002169f87a118302578
Angel de la Torre <angel.dlt.1996@gmail.com>
2022-01-11 11:10:40 -0800

    feat(db_commit): add save func

--- db/db_commit.ts
+++ db/db_commit.ts
@@ -1,4 +1,8 @@
 import { Commit } from "@prisma/client";
+import prisma from "../prisma/client"
+
+import logger from '../src/logger';
+const log = logger('db_commit');
 
 export default class CommitDB {
   commit: Commit
@@ -15,6 +19,32 @@ export default class CommitDB {
       committed_at: 0,
       pushed_at: null,
       pull_request_id: null,
+    }
+   }
+
+  async save(): Promise<void> {
+    if (this.commit.sha !== null) {
+      try {
+        this.commit = await prisma.commit.upsert({
+          where: {
+            commit_event_id: this.commit.commit_event_id,
+          },
+          update: this.commit,
+          create: this.commit
+        })
+      } catch (e) {
+        log.error(
+          "Failed to save Commit %O\n\t%s",
+          this.commit,
+          e
+        )
+        throw e
+      }
+    } else {
+      log.error(
+        "Failed to save Commit because it has not been intiliazed correctly %O",
+        this.commit,
+      )
     }
   }
 }
