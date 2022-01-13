8b4b4d96bc906d1f6a9ddb60faf4ec39c1b7e09f
Angel de la Torre <angel.dlt.1996@gmail.com>
2022-01-13 11:25:22 -0800

    feat(db_pull_history): added pull_history class

---
+++ db/db_pull_history.ts
@@ -0,0 +1,40 @@
+import { Commit, PullRequestHistory, pull_request_history_event } from "@prisma/client";
+import prisma from "../prisma/client"
+
+import CommitDB from '../db/db_commit'
+import Pull from "./db_pull";
+
+import logger from '../src/logger';
+const log = logger('db_pull_history');
+
+export default class PullHistoryRecorder {
+  pull_records: PullRequestHistory[] = [];
+  pull_request_id: string;
+  current_commit: CommitDB;
+
+  constructor (pull_request_id?: string, current_commit?: CommitDB) {
+    this.pull_request_id = pull_request_id ?? '';
+    this.current_commit = current_commit ?? new CommitDB();
+  }
+
+  async save(): Promise<void> {
+    log.data('Saving events %O', this.pull_records)
+    await Promise.allSettled(this.pull_records.map(async event => {
+      try {
+        await prisma.pullRequestHistory.upsert({
+          where: {
+            date_pull_request_id_event: {
+              date: event.date,
+              pull_request_id: event.pull_request_id,
+              event: event.event
+            }
+          },
+          update: event,
+          create: event
+        })
+      } catch (err) {
+        log.error('Failed to save event %O\n%s', event, err)
+      }
+    }))
+  }
+}
