5fcebbeb12c3d9337dc1b87381f82222c9ca0136
Angel de la Torre <angel.dlt.1996@gmail.com>
2022-01-13 15:28:28 -0800

    feat(moment): install and use moment
    The date-and-time package, while lightweight, was difficult to work with
    Therefore, it is worth switching over to using moment.js.
    
    In addition, GitHub uses ISO dates so we will need to often convert dates
    from ISO to Unix

--- scripts/utils.ts
+++ scripts/utils.ts
@@ -1,9 +1,14 @@
 import date from 'date-and-time';
+import moment from 'moment';
 
 export const utils = {
   getDates(): [today:number,yesterday:number] {
     const today = Math.floor(new Date().setHours(0, 0, 0, 0) / 1000);
     const yesterday = Math.floor(date.addDays(new Date(), -1).setHours(0, 0, 0, 0) / 1000);
     return [today, yesterday];
+  },
+
+  getUnixTimeFromISO(iso_date: string): number{
+    return moment(iso_date).unix()
   }
 }
