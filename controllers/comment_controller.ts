22fb4d07d7cbdf1f6d3859ceb838f64a27367399
Angel de la Torre <angel.dlt.1996@gmail.com>
2022-01-24 16:47:54 -0800

    refactor(comment_controller): parse all comments
    Instead of only being able to parse comments from regular PR comments,
    generalize the functions to also accept comments from Reviews.
    
    Reviews, however, have to types of comments that can be left.
    One is the general review comment and the second is granular comments
    on portions of the code:
    https://github.com/iFixit/ifixit/pull/41449#pullrequestreview-858901223

--- controllers/comment_controller.ts
+++ controllers/comment_controller.ts
@@ -1,30 +1,30 @@
 import logger from '../src/logger'
 const log = logger('comment_controller')
 
-import { IssueComment} from "@octokit/graphql-schema"
+import { Actor, Maybe, Scalars} from "@octokit/graphql-schema"
 
 import config from '../config/config'
 const { qa_team, signatures } = config
 
-export function parseComment(comment: IssueComment, pull_request_author: string): { qaed: boolean, dev_block: boolean | null, interacted: boolean }{
+export function parseComment(comment_bodyText: Scalars['String'], comment_author: Maybe<Actor> | undefined, pull_request_author: string): { qaed: boolean, dev_block: boolean | null, interacted: boolean }{
   return {
-    qaed: isQAed(comment),
-    dev_block: isDevBlocked(comment),
-    interacted: isInteracted(comment,pull_request_author)
+    qaed: isQAed(comment_bodyText),
+    dev_block: isDevBlocked(comment_bodyText),
+    interacted: isInteracted(comment_bodyText, comment_author, pull_request_author)
   }
 }
 
 
-function isQAed(comment: IssueComment): boolean {
+function isQAed(comment_bodyText: Scalars['String']): boolean {
   const regex = new RegExp(signatures.QA + signatures.emoji, 'i')
-  return regex.test(comment.bodyText)
+  return regex.test(comment_bodyText)
 }
 
 // Checking for dev_block or un_dev_block, otherwise return null
-function isDevBlocked(comment: IssueComment): boolean | null {
+function isDevBlocked(comment_bodyText: Scalars['String']): boolean | null {
   const tag = signatures.tags.find(tag => {
     const regex = new RegExp(tag.regex + signatures.emoji, 'i')
-    return regex.test(comment.bodyText)
+    return regex.test(comment_bodyText)
   })
   if (tag) {
     return tag.state
@@ -32,6 +32,6 @@ function isDevBlocked(comment: IssueComment): boolean | null {
   return null
 }
 
-function isInteracted(comment: IssueComment, pull_request_author: string): boolean {
-  return qa_team.includes(comment.author?.login ?? '') && pull_request_author !== comment.author?.login
+function isInteracted(comment_bodyText: Scalars['String'], comment_author: Maybe<Actor> | undefined, pull_request_author: string): boolean {
+  return qa_team.includes(comment_author?.login ?? '') && pull_request_author !== comment_author?.login
 }
