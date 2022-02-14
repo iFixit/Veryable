cd6d66030e55e28a04289cf3aaca68c4dfea687b
Angel de la Torre <angel.dlt.1996@gmail.com>
2022-02-14 14:30:59 -0800

    refactor(db_day): refine responsibility
    Now that we have a record of events with dates, we can backfill the Day table.
    This also means we can remove a lot of the logic that was
    associated with
    the active record design.

--- db/db_day.ts
+++ db/db_day.ts
@@ -11,71 +11,25 @@ export default class DayMetric
 {
   metrics: Day
 
-  yesterday?: Day | null;
-  constructor()
-  {
-    this.metrics = {
-      pull_count: 0,
-      pulls_added: 0,
-      pulls_interacted: 0,
-      unique_pulls_added: 0,
-      date: 0
-    };
+  constructor (metrics: Day) {
+    this.metrics = utils.deepCopy(metrics)
   }
 
-  // Initial the day
-  async init(): Promise<void> {
-    const [today, yesterday] = utils.getDates();
-    this.yesterday = await prisma.day.findFirst({ where: { date: yesterday } })
-
-     log.data(`Today's values are ${today} and yesterday is ${this.yesterday?.date}`);
-    if (this.yesterday) {
-      this.metrics.pull_count = this.yesterday.pull_count
-    }
-    this.metrics = await prisma.day.findFirst({ where: { 'date': today }}) || {...this.metrics, date: today};
-
-    log.data(`Day Data ${JSON.stringify(this.metrics, null, 2)}`);
-  }
-
-  // Insert the new Day in the table and if it exists Update the values accordingly
-  async save(): Promise<void>
-  {
-    if ( this.isNotToday() )
-    {
-     const [ today ] = utils.getDates();
-      this.yesterday = this.metrics;
-      this.metrics = {
-        pull_count: this.yesterday?.pull_count || 0,
-        pulls_added: 0,
-        pulls_interacted: 0,
-        unique_pulls_added: 0,
-        date: today
-      };
-    }
-    try
-    {
+  async save(): Promise<void> {
+    try {
       await prisma.day.upsert({
         where: { date: this.metrics.date },
         update: this.metrics,
         create: this.metrics
       })
-    } catch ( e )
-    {
-      log.error( "Failed to save Day " + e );
+    } catch (e) {
+      log.error('Failed to save Day %d\n\t%s', this.metrics.date, e);
     }
-  }
-
-  setNewValues(new_day_values: Day): void {
-    this.metrics = { ...new_day_values, date: this.metrics.date}
   }
 
   getDayValues(): Day
   {
-    return { ...this.metrics };
-  }
-
-  private isNotToday(): boolean{
-    return this.metrics.date !== utils.getDates()[0]
+    return utils.deepCopy(this.metrics);
   }
 }
 
