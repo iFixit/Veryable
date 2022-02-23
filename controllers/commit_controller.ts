c83780915aa59ae45b90ab0f5eb03dc16864d9e1
Angel de la Torre <angel.dlt.1996@gmail.com>
2022-02-23 12:25:20 -0800

    fix(parse_commit): use any to handle schema diff
    GraphQL throws an error due to GitHub's conflicting commit types for
    PullRequestCommit and PullRequestReview.
    This was resolved by adding an alias to those fields in the GraphQL
    query 0e2f1a83712a3fc61ed952cc41aa943bd5f4b071
    
    However, the object we get back is not really of type PullRequestCommit
    because the returned object has the alias name instead of `commit`.
    Instead of creating a new PullRequestCommit type, just use any for now.

--- controllers/commit_controller.ts
+++ controllers/commit_controller.ts
@@ -4,22 +4,21 @@ import { Commit, commit_ci_status } from "@prisma/client"
 
 import { utils } from "../scripts/utils"
 
-import { PullRequestCommit } from '@octokit/graphql-schema';
-
 import logger from '../src/logger';
 const log = logger('commit_controller');
 
-export function parseCommit(pull: Pull, github_commit: PullRequestCommit): CommitDB {
+
+export function parseCommit(pull: Pull, github_commit: any): CommitDB {
   const commit: Commit = {
     commit_event_id: github_commit.id,
-    sha: github_commit.commit.oid,
+    sha: github_commit.pr_commit.oid,
     qa_ready: false,
     interacted: false,
     dev_blocked: false,
     qa_stamped: false,
-    ci_status: github_commit.commit.status?.state || null,
-    committed_at: utils.getUnixTimeFromISO(github_commit.commit.committedDate),
-    pushed_at: github_commit.commit.pushedDate ? utils.getUnixTimeFromISO(github_commit.commit.pushedDate) : null,
+    ci_status: github_commit.pr_commit.status?.state || null,
+    committed_at: utils.getUnixTimeFromISO(github_commit.pr_commit.committedDate),
+    pushed_at: github_commit.pr_commit.pushedDate ? utils.getUnixTimeFromISO(github_commit.pr_commit.pushedDate) : null,
     pull_request_id: pull.getID()
   }
 
